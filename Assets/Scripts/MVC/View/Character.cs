16e4795ded551f97bc6b3e6587715b540a865226
Daniel Arredondo <daniel7nov@hotmail.com>
2017-11-13 17:54:19 -0500

    Re-estructurando un poco Vista

---
+++ Assets/Scripts/MVC/View/Character.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+	protected Animator anim;	
+
+	/*void Start(){
+		anim=GetComponent<Animator>();
+	}*/
+
+	static void Update(){
+		Debug.Log("Funciono aquí");
+	}
+}
---
+++ Assets/Scripts/MVC/View/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+	Vector3 startPos=new Vector3(0,0,0);
+	Vector3 endPos=new Vector3(0,0,0);
+
+	void NotAttack(){
+		anim.SetBool("Attack",false);
+	}
+
+	void Attack(){
+		anim.SetBool("Attack",true);
+	}
+
+	public void Movement(){
+		Vector3.Slerp(startPos,endPos,1f);
+	}
+
+	void Jump(){
+
+	}
+
+}
