dbd0de7decc8ce7697b7555c2e0770f4dd00bf55
Daniel Arredondo <daniel7nov@hotmail.com>
2017-11-23 13:12:26 +0100

    Vista: UI_ViewManager

---
+++ Assets/Scripts/MVC/View/UI_ViewManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_ViewManager : MonoBehaviour {
+
+    Animator anim;
+
+    void Start() {
+        //anim = GetComponentInChildren<Animator>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    //Setting Button
+    public void HereSettingButton()
+    {
+        anim.SetTrigger("SettingsDown");
+    }
+    public void ExitSettingButton()
+    {
+        anim.SetTrigger("SettingsUp");
+    }
+
+    //Score Button
+    public void HereScoreButton()
+    {
+        anim.SetTrigger("ScoreUp");
+    }   
+
+    public void ExitScoreButton()
+    {
+        anim.SetTrigger("ScoreDown");
+    }
+
+    //Score Name
+    public void HereScoreNameButton()
+    {
+        anim.SetTrigger("NameEnter");
+    }
+
+    public void ExitScoreNameButton() {
+        anim.SetTrigger("NameExit");
+    }
+
+    //ScenesLoders
+    public void NextScene(int index) {
+        SceneManager.LoadScene(index);
+    }
+
+    
+
+}
