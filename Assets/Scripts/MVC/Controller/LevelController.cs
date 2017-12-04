092c283130a2ef4f4b6261c04c64999cfdde33a9
muertx <gonzalo.cordero.97@hotmail.com>
2017-12-04 05:49:08 -0500

    Agregado script LevelController.

---
+++ Assets/Scripts/MVC/Controller/LevelController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] objects = new GameObject[4];
+
+
+
+	// Use this for initialization
+	void Start () {
+        objects[0] = GameObject.Find("IniModuDownLeft(Clone)");
+        objects[1] = GameObject.Find("ModuUpLeft(Clone)");
+        objects[2] = GameObject.Find("EndModuUpRight(Clone)");
+        objects[3] = GameObject.Find("ModuDownRight(Clone)");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            Destroy(objects[0]);
+            Destroy(objects[1]);
+            Destroy(objects[2]);
+            Destroy(objects[3]);
+        }
+    }
+
+}
